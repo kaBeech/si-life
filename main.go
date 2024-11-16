09bbb74d495fc0f52aeb9d0f7930dd2b4d3c2e62
kaBeech <contact@kaBeech.com>
2024-11-15 22:27:34 -0800

    Add Create SiFloor route

--- main.go
+++ main.go
@@ -18,8 +18,28 @@ func main() {
 
   siFloors := []SiFloor{}
 
+  // Index route
   app.Get("/", func(c *fiber.Ctx) error {
-    return c.Status(200).JSON(fiber.Map{"msg": "Welcome to Si-Life!"})
+    return c.Status(200).JSON(fiber.Map{"msg": "Welcome to  SiLife!"})
+  })
+
+  // Create a new Si-Floor
+  app.Post("/api/sifloor", func(c *fiber.Ctx) error {
+    siFloor := &SiFloor{}
+  
+
+    if err := c.BodyParser(siFloor); err != nil {
+      return err
+    }
+
+    if siFloor.Height == 0 || siFloor.Width == 0 {
+      return c.Status(400).JSON(fiber.Map{"error": "Height and Width are required"})
+    }
+
+    siFloor.ID = len(siFloors) + 1
+    siFloors = append(siFloors, *siFloor)
+
+    return c.Status(201).JSON(siFloor)
   })
 
   // Start the server, else log errors
