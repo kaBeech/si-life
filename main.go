ad9364fab365a541514bb73ba978b0cdc6676aa7
kaBeech <contact@kaBeech.com>
2024-11-15 20:11:14 -0800

    Add boilerplate Go app

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+ 
+import (
+  "net/http"
+
+  "github.com/gin-gonic/gin"
+)
+ 
+func main() {
+  router := gin.Default()
+ 
+  // Create API route group
+  api := router.Group("/api")
+  {
+    // Add routes and define route handler functions
+    api.GET("/si-life", func(ctx *gin.Context) {
+      ctx.JSON(200, gin.H{"msg": "Welcome to Si-Life!"})
+    })
+  }
+ 
+  // Handle 404s
+  router.NoRoute(func(ctx *gin.Context) { ctx.JSON(http.StatusNotFound, gin.H{}) })
+ 
+  // Start the server
+  router.Run(":8080")
+}
