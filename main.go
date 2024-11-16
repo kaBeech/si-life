5efcb8d62e31b03ed927818b4aaabb2c23c33d48
kaBeech <contact@kaBeech.com>
2024-11-15 22:26:41 -0800

    Add SiFloor type

--- main.go
+++ main.go
@@ -6,9 +6,17 @@ import (
 
   "github.com/gofiber/fiber/v2"
 )
- 
+
+type SiFloor struct {
+  ID      int    `json:"id"`
+  Height  int    `json:"height"`
+  Width   int    `json:"width"`
+}
+
 func main() {
   app := fiber.New()
+
+  siFloors := []SiFloor{}
 
   app.Get("/", func(c *fiber.Ctx) error {
     return c.Status(200).JSON(fiber.Map{"msg": "Welcome to Si-Life!"})
