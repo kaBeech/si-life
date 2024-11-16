4b515abbad80fcf48c5349249482160495beeb2b
kaBeech <contact@kaBeech.com>
2024-11-15 21:54:20 -0800

    Add initial route

--- main.go
+++ main.go
@@ -9,19 +9,11 @@ import (
  
 func main() {
   app := fiber.New()
- // 
- //  // Create API route group
- //  api := router.Group("/api")
- //  {
- //    // Add routes and define route handler functions
- //    api.GET("/si-life", func(ctx *gin.Context) {
- //      ctx.JSON(200, gin.H{"msg": "Welcome to Si-Life!"})
- //    })
- //  }
- // 
- //  // Handle 404s
- //  router.NoRoute(func(ctx *gin.Context) { ctx.JSON(http.StatusNotFound, gin.H{}) })
- 
+
+  app.Get("/", func(c *fiber.Ctx) error {
+    return c.Status(200).JSON(fiber.Map{"msg": "Welcome to Si-Life!"})
+  })
+
   // Start the server, else log errors
   log.Fatal(app.Listen(":4000"))
   fmt.Println("Server running on port 4000")
