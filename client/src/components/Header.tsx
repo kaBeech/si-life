cdac0868d1d24ede0512d2103bc3500741d6e97d
kaBeech <contact@kaBeech.com>
2024-11-16 16:04:15 -0800

    Add boilerplate header

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import NavBar from "./NavBar"
+
+export default () => {
+  return (
+    <header className="flex row justifySpaceBetween gap2">
+      <h1>SiLife</h1>
+      <NavBar />
+    </header>
+  )
+}
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+export default () => {
+  return (
+    <nav >
+      <ul className="flex row gap2">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
